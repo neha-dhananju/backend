2b519acc741cb82816cd86525ec95e55c3b67a22
neha-dhananju <neha02dhananju@gmail.com>
2026-01-17 11:43:52 +0530

    Clean repo: remove duplicate root backend files

--- app/services/video_service.py
+++
@@ -1,8 +0,0 @@
-from app.integrations.youtube_client import YouTubeClient
-
-client = YouTubeClient()
-
-
-class VideoService:
-    def get_video(self, recipe_title: str):
-        return client.search_recipe_video(recipe_title)
--- app/utils/exceptions.py
+++
@@ -1,5 +0,0 @@
-from fastapi import HTTPException
-
-class AppException(HTTPException):
-    def __init__(self, status_code: int, message: str):
-        super().__init__(status_code=status_code, detail=message)
